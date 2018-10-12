e945466ecef83daf43e6fc77e8813e964109fea9
Manraaj Nijjar <ManraajNijjar@gmail.com>
2018-10-12 13:17:53 -0400

    Added second implementation to AutoComplete not completely finished but most the class design is essentially finished

--- October2018/autoComplete.java
+++ October2018/autoComplete.java
@@ -22,3 +22,50 @@ public class autoComplete {
 	}
 
 }
+
+class AlphaTree{
+	boolean isRoot;
+	boolean isWord;
+	public char nodeValue;
+	public AlphaTree[] subTrees = new AlphaTree[26];
+	int emptyTree = 0;
+	AlphaTree(){
+		isRoot = true;
+	}
+	AlphaTree(char check){
+		isRoot = false;
+		nodeValue = check;
+	}
+	void insert(String value) {
+		char currValue = value.charAt(0);
+		int index = searchLevel(currValue);
+		if(index != -1) {
+			subTrees[index].insert(value.substring(1));
+		} else {
+			AlphaTree newTree = new AlphaTree(currValue);
+			newTree.insert(value.substring(1));
+			subTrees[emptyTree] = newTree;
+			emptyTree++;
+		}
+		if(value.length() == 1) {
+			isWord = true;
+		}
+	}
+	
+	String[] search(String searchPhrase) {
+		String[] results = new String[10];
+		int next = searchLevel(searchPhrase.charAt(0));
+		if(next != -1) {
+			//Recursively search through the system after getting to the end of the search phrase
+		} 
+		return results;
+		
+	}
+	
+	int searchLevel(char value) {
+		for(int x = 0; x < subTrees.length; x++) {
+			if(subTrees[x].nodeValue == value) return x;
+		}
+		return -1;
+	}
+}
